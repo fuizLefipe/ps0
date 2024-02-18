a0332dd2a4ac04597795ae4f6edf335130e66393
fuizLefipe <158861494+fuizLefipe@users.noreply.github.com>
2024-02-18 18:22:37 -0300

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 15:23:44 2024
+
+@author: lenovo
+"""
+import numpy
+
+x = int(input("Enter number x: "))
+
+y = int(input("Enter number y: "))
+
+print("x**y =", x**y)
+
+print("log(x) =", numpy.log2(x))
